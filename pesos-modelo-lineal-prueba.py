358995ae7d09c5cd2e7757026400e18922ef4360
Xus72 <jexusth@hotmail.com>
2020-04-16 12:16:42 +0200

    add solve error_cuadratico_medio

--- pesos-modelo-lineal-prueba.py
+++ pesos-modelo-lineal-prueba.py
@@ -18,8 +18,10 @@
 # Comprobar también si el mejor vector de pesos con respecto a esos 90 ejemplos
 # es también el mejor con respecto a los diez últimos.  
 
+import csv
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
+
 
 w1=[-1.29534026,  1.23835128,  0.12877138, -2.96549057, -0.35841744,
     2.82746578,  2.21482984, -1.65336872, -0.47153383, -0.44702348,
@@ -42,6 +44,50 @@ w5=[ 3.75038563, -1.05615946, -1.46526707,  2.11298963, -0.5708379 ,
              1.55773606, 1.11062204,  2.65842872,  3.13673974, -1.91793153,
             -1.83749571]
 
+#Resuelto con python
+def lee_datos(fichero):
+    with open(fichero,newline="") as f:
+        lectordatos = csv.reader(f,delimiter=',',quoting=csv.QUOTE_NONNUMERIC)
+        datos,valores = [],[]
+        for fila in lectordatos:
+            datos.append([1]+fila[:-1])
+            valores.append(fila[-1])
+    return datos,valores
+
+datos,valores = lee_datos("datos-aia-prueba.csv")
+
+X_train = datos[:90]
+y_train = valores[:90]
+X_test = datos[90:]
+y_test = valores[90:]
+
+def combinacion_lineal(w,x):
+    return sum(wi*xi for wi,xi in zip(w,x))
+
+def error_cuadratico_medio(X,y,w):
+    return sum((yj-combinacion_lineal(w,Xj))**2 for Xj,yj in zip(X,y))/len(X)
+
+print("Set de entrenamiento")
+print(error_cuadratico_medio(X_train,y_train,w1))
+print(error_cuadratico_medio(X_train,y_train,w2))
+print(error_cuadratico_medio(X_train,y_train,w3))
+print(error_cuadratico_medio(X_train,y_train,w4))
+print(error_cuadratico_medio(X_train,y_train,w5))
+
+print("\nSet de test")
+print(error_cuadratico_medio(X_test,y_test,w1))
+print(error_cuadratico_medio(X_test,y_test,w2))
+print(error_cuadratico_medio(X_test,y_test,w3))
+print(error_cuadratico_medio(X_test,y_test,w4))
+print(error_cuadratico_medio(X_test,y_test,w5))
+
+#Descenso estocástico por el gradiente
+def aprende_pesos(train,valor,rango_inicial,n_epochs,tasa=0.1):
+    #usar rango -3,3
+    return w
+
+#Resuelto con librerias ML
+
 df = pd.read_csv('datos-aia-prueba.csv', header=None)
 
 set = df.drop(columns=[10])
@@ -52,10 +98,12 @@ train_set = set.iloc[:90]
 #Declaro el conjunto de test cogiendien las 10 ultimas filas del dataset
 test_set = set.iloc[91:100]
 
+theta = train_set[0] * np.transpose(w1).shape
 
+error = 0
 
-print(set)
-print(train_set)
-print(test_set)
+#def f_err_cuad (w, x):
+#     return (x[-1] - (w[0] + np.dot(np.array(w[1:]), np.array(x[:-1]))))**2
+
 
 
